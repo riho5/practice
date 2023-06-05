87a2611f5747c4ed2ce763e09d80e4983664efa4
riho5 <91946224+riho5@users.noreply.github.com>
2023-06-05 16:11:19 +0900

    write comment in __init__.py

---
+++ __init__.py
@@ -0,0 +1,13 @@
+#add
+#git remote add origin https://github.com/riho5/practice.git  
+#git branch -M main
+#git push -u origin main
+
+#commit
+#git commit -m "comment"
+
+#push
+#git push origin main
+
+#pull 
+#git pull origin main
