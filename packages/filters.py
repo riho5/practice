ee68ecd06edd9eabc0a37be39c6eafa58d9f409f
riho5 <91946224+riho5@users.noreply.github.com>
2023-06-08 14:02:22 +0900

    make directory

---
+++ packages/filters.py
@@ -0,0 +1,41 @@
+import numpy as np
+from scipy import signal
+import pandas as pd
+import matplotlib.pyplot as plt
+
+ # -------------------------------------------------
+def lowpass(y,n): #位相はズレる
+    Res = np.array([0])
+    dt = 0.02
+
+    for yn,yn_1 in zip(y[1:],y[:-1]):
+        res = (dt*(yn+yn_1)+(2*n-dt)*Res[-1])/(2*n+dt)
+        Res = np.append(Res,res)
+    return Res
+
+def get_lowpass(y,n = 0.05):
+    return lowpass(y,n)
+    
+ # -------------------------------------------------
+def highpass(y,n):
+    Res = np.array([0])
+    dt = 0.02
+
+    for yn,yn_1 in zip(y[1:],y[:-1]):
+        res = (2*n*(yn-yn_1)+(2*n-dt)*Res[-1])/(2*n+dt)
+        Res = np.append(Res,res)
+    return Res
+    
+ # -------------------------------------------------
+def complementaryfilter(a,g,n = 0.05):
+
+    # (a : angle [rad], g : gyro [rad/deg], n = default 0.05 (1/20))
+    # return -> angle [deg]
+
+    Res = np.array([0])
+    ts = 0.02 #timestep
+    for an,an_1,gn,gn_1 in zip(a[1:],a[:-1],g[1:],g[:-1]):
+        res = (2*n-ts)/(2*n+ts)*Res[-1]+ts/(2*n+ts)*(an+an_1)+ts*n/(2*n+ts)*(gn+gn_1)
+        Res = np.append(Res,res)
+    
+    return np.degrees(Res)
